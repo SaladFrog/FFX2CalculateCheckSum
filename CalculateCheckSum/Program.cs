a175407953e4e68819706281a3d2285bbff190a6
Tobias Löfvendahl <tobias.lofvendahl@pm.me>
2022-06-07 11:58:41 +0200

    Add project files.

---
+++ CalculateCheckSum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CalculateCheckSum
+{
+    public class Program
+    {
+        private static MemoryStream? ms;
+        private static BinaryReader? br;
+        private static BinaryWriter? bw;
+        private static readonly int crcOffset = 0xd424;
+
+        public static void Main(string[] args)
+        {
+            string saveFile = $"{args}";
+            MemoryStream saveStream = new MemoryStream(File.ReadAllBytes(saveFile));
+            ms = saveStream;
+            br = new BinaryReader(ms);
+            bw = new BinaryWriter(ms);
+
+            bw.Write(crcOffset, 0);
+            Crc16_CCITT hasher = new Crc16_CCITT();
+            {
+                br.BaseStream.Seek(0x40, SeekOrigin.Begin);
+                hasher.ComputeHash(br.ReadBytes(crcOffset + 4 - 0x40));
+                bw.Write(0x1a, (short)hasher.Value);
+                bw.Write(crcOffset, (short)hasher.Value);
+            }
+
+            using (BinaryWriter fw = new BinaryWriter(File.Open(saveFile, FileMode.Create)))
+            {
+                fw.Write(ms.ToArray());
+            }
+        }
+    }
+}
+
---
+++ CalculateCheckSum/Utils.cs
@@ -0,0 +1,38 @@
+﻿namespace CalculateCheckSum
+{
+    public static class Utils
+    {
+        public static double RadianToDegree(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+
+        public static byte[] BoolsToBytes(bool[] bools)
+        {
+            var bytes = new byte[bools.Length / 8];
+            for (int b = 0; b < bytes.Length; b++)
+            {
+                for (int bit = 0; bit < 8; bit++)
+                {
+                    if (bools[b + bit])
+                        bytes[b] &= (byte)Math.Pow(2, bit);
+                }
+            }
+            return bytes;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void Write(this BinaryWriter bw, bool[] bools)
+        {
+            bw.Write(Utils.BoolsToBytes(bools));
+        }
+
+        public static void Write(this BinaryWriter bw, int offset, short value)
+        {
+            bw.BaseStream.Seek(offset, SeekOrigin.Begin);
+            bw.Write(value);
+        }
+    }
+}
